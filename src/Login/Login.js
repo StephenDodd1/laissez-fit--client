91f163fdca7c0429969ffac6598051bc9f632d38
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-08-25 08:39:03 -0500

    update to css

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -15,34 +15,40 @@ export default class Login extends Component {
     );
     console.log(this.context.user_id);
     return (
-      <div className="background">
-        <div className="vertical-elements">
-          <h2 className="login-signup-header">Login</h2>
-          <div className="centered-form">
-            <form className="credentials-box" onSubmit={this.props.submitLogin}>
-              {renderDemoOrUser}
-              <div className="input-label">
-                <label htmlFor="username">Username</label>
-                <input
-                  id="username"
-                  className="login-signup-inputs"
-                  name="username"
-                />
-              </div>
-              <div className="input-label">
-                <label htmlFor="password">Password</label>
-                <input
-                  id="password"
-                  className="login-signup-inputs"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                />
-              </div>
-              <button>Login</button>
-            </form>
+      <div>
+        <div className="background">
+          <div className="vertical-elements">
+            <h2 className="login-signup-header">Login</h2>
+            <div className="centered-form">
+              <form
+                className="credentials-box"
+                onSubmit={this.props.submitLogin}
+              >
+                {renderDemoOrUser}
+                <div className="input-label">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    id="username"
+                    className="login-signup-inputs"
+                    name="username"
+                  />
+                </div>
+                <div className="input-label">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    id="password"
+                    className="login-signup-inputs"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                  />
+                </div>
+                <button>Login</button>
+              </form>
+            </div>
           </div>
         </div>
+        <div className="background"></div>
       </div>
     );
   }
