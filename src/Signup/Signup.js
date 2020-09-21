f4163c73e060ad1de362f5ab190c56c1e4dd8be1
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-09-21 08:31:10 -0500

    update error message

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -57,7 +57,7 @@ export default class Signup extends Component {
     }).then((res) => {
       if (!res.ok) {
         let response = res.json();
-        return response.error
+        return console.log(response.error)
       }
       this.setState({ userCreated: true });
       return res.json();
