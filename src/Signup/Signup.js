ec0d2156246b6950d514d2a07efb3721d11c3517
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-09-21 10:44:09 -0500

    update error message

--- src/Signup/Signup.js
+++ src/Signup/Signup.js
@@ -64,7 +64,7 @@ export default class Signup extends Component {
         }
       })
       .then((response) => {
-        console.log(response.json())
+        console.log(response)
       });
   };
   render() {
