b9ef8aecbd3871377bbe463e9cce04335a613509
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-10-02 23:33:53 -0500

    update login to redirect

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
         this.login(data.data.user.id);
         this.setUsername(data.data.user.username);
         this.setUserName(data.data.user.name);
-      });
+      }).then((res) => this.props.history.push("/Demo"));
   };
   render() {
     const renderLoginOrLogout =
