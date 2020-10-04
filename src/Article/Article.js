2b59a33518893c0b4b0ee8acbcd9cb68ced3db7a
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-10-04 13:58:37 -0500

    updated data reset on forms

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -11,7 +11,7 @@ export default class Article extends Component {
   }
 
   componentDidMount() {
-    console.log(window.location.pathname)
+    console.log('pathname', window.location.pathname)
     const URL = `${config.API_URL}/api/article/${
       window.location.pathname.split("/")[2]
     }`;
